d3770d6e589e7f1feef6b0b02c055a7edb7ba027
agipranadika <125530228+agipranadika@users.noreply.github.com>
2025-07-08 11:34:12 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -2,8 +2,14 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export default function handler(req, res) {
-  const filePath = join(process.cwd(), 'data.json');
-  const jsonData = readFileSync(filePath, 'utf-8');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(jsonData);
+  try {
+    // arahkan ke folder yang benar (misal 'pahlawan_nasional.json')
+    const filePath = join(process.cwd(), 'json', 'pahlawan_nasional.json');
+    const jsonData = readFileSync(filePath, 'utf-8');
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(jsonData);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read JSON file', detail: error.message });
+  }
 }
